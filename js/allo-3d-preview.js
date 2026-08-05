37379a694ac3fc5835bacaabffc587be86b8cc40
Smiley <nick.j.peterson9@gmail.com>
2026-08-04 19:16:23 -0500

    trike 3d

--- js/allo-3d-preview.js
+++ js/allo-3d-preview.js
@@ -21,7 +21,8 @@ const viewerModules = Object.freeze({
   pachy: "./dinos/pachycephalosaurus-3d-preview.js",
   stego: "./dinos/stegosaurus-3d-preview.js",
   tenonto: "./dinos/tenontosaurus-3d-preview.js",
-  trex: "./dinos/tyrannosaurus-3d-preview.js"
+  trex: "./dinos/tyrannosaurus-3d-preview.js",
+  trike: "./dinos/triceratops-3d-preview.js"
 });
 
 const loadedViewers = new Map();
